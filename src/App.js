d41ee4adc990ba60d772ec44398edda278e9c8c3
Ons Janeb <onesjaneb@gmail.com>
2024-03-24 16:45:04 +0100

    create addcours component interface

--- src/App.js
+++ src/App.js
@@ -9,39 +9,15 @@ import EmploisEtudiant from "./components/Global/Emplois";
 import Actualites from "./components/Global/Actualites";
 import Profil from "./components/Global/Profil/Profil";
 
-<<<<<<< HEAD
 import PresenceEtudiant from "./components/Etudiant/Dashboard/Presence/Presence";
 import NoteEtudiant from "./components/Etudiant/Dashboard/Note/Note";
-import CoursEtudiant from "./components/Etudiant/Dashboard/Cours/Cours";
+import CoursEtudiant from "./components/Etudiant/Dashboard/Cours/DashboardCoursEtudiant";
 import VieEstudiantine from "./components/Etudiant/Dashboard/VieEstudiantine/VieEstudiantine";
 import AuthEtudiant from "./components/Etudiant/Registration/AuthEtudiant";
 import DashboardEtudiant from "./components/Etudiant/Dashboard/Dashboard";
-=======
-import PresenceEtudiant from './components/Etudiant/Dashboard/Presence/Presence';
-import NoteEtudiant from './components/Etudiant/Dashboard/Note/Note';
-import DashboardCoursEtudiant from './components/Etudiant/Dashboard/Cours/DashboardCoursEtudiant';
-import VieEstudiantine from './components/Etudiant/Dashboard/VieEstudiantine/VieEstudiantine';
-import AuthEtudiant from './components/Etudiant/Registration/AuthEtudiant'
-import DashboardEtudiant from './components/Etudiant/Dashboard/Dashboard';
-
-import PresenceEnseignant from './components/Enseignant/Dashboard/Presence/Presence';
-import CoursEnseignant from './components/Enseignant/Dashboard/Cours/Cours';
-import DashboardEnseignant from './components/Enseignant/Dashboard/Dashboard';
-
-import DashboardAdmin from './components/Admin/Dashboard/Dashboard';
-import GestionAct from './components/Admin/Dashboard/GestionAct/GestionAct';
-import GestionBib from './components/Admin/Dashboard/GestionBib/GestionBib';
-import GestionCalendar from './components/Admin/Dashboard/GestionCalendar/GestionCalendar';
-import GestionClub from './components/Admin/Dashboard/GestionClub/GestionClub';
-import GestionEmploi from './components/Admin/Dashboard/GestionEmploi/GestionEmploi';
-import GestionNote from './components/Admin/Dashboard/GestionNote/GestionNote';
-import GestionPresence from './components/Admin/Dashboard/GestionPresence/GestionPresence';
-import GestionProfil from './components/Admin/Dashboard/GestionProfil/GestionProfil';
-import DashboardCours from './components/Etudiant/Dashboard/Cours/DashboardCoursEtudiant';
->>>>>>> 1426ec7e16bdb441b3101bab392881f38458d100
 
 import PresenceEnseignant from "./components/Enseignant/Dashboard/Presence/Presence";
-import CoursEnseignant from "./components/Enseignant/Dashboard/Cours/Cours";
+import CoursEnseignant from "./components/Enseignant/Dashboard/Cours/AddCours";
 import DashboardEnseignant from "./components/Enseignant/Dashboard/Dashboard";
 
 import DashboardAdmin from "./components/Admin/Dashboard/Dashboard";
@@ -75,45 +51,12 @@ class App extends Component {
             <Route path="/coursEtudiant" Component={CoursEtudiant}></Route>
             <Route path="/vieEstudiantine" Component={VieEstudiantine}></Route>
 
-<<<<<<< HEAD
             {/*Etudiant et enseignant*/}
             <Route path="/calendar" Component={Calendar}></Route>
             <Route path="/bibliotheque" Component={Bibliotheque}></Route>
             <Route path="/emplois" Component={EmploisEtudiant}></Route>
             <Route path="/actualites" Component={Actualites}></Route>
             <Route path="/profil" Component={Profil}></Route>
-=======
-              {/*Etudiant */}
-              <Route path='/' Component={AuthEtudiant}></Route>
-              <Route path='/dashboardEtudiant' Component={DashboardEtudiant}></Route>
-              <Route path='/presenceEtudiant' Component={PresenceEtudiant}></Route>
-              <Route path='/noteEtudiant' Component={NoteEtudiant}></Route>
-              <Route path='/coursEtudiant' Component={DashboardCoursEtudiant}></Route>
-              <Route path='/vieEstudiantine' Component={VieEstudiantine}></Route>
-
-              {/*Etudiant et enseignant*/}
-              <Route path='/calendar' Component={Calendar}></Route>
-              <Route path='/bibliotheque' Component={Bibliotheque}></Route>
-              <Route path='/emplois' Component={EmploisEtudiant}></Route>
-              <Route path='/actualites' Component={Actualites}></Route>
-              <Route path='/profil' Component={Profil}></Route>
-
-              {/*Enseignant*/}
-              <Route path='/dashboardEnseignant' Component={DashboardEnseignant}></Route>
-              <Route path='/presenceEnseignant' Component={PresenceEnseignant}></Route>
-              <Route path='/coursEnseignant' Component={CoursEnseignant}></Route>
-
-              {/*Admin */}
-              <Route path='/dashboardAdmin' Component={DashboardAdmin}></Route>
-              <Route path='/GestionActualites' Component={GestionAct}></Route>
-              <Route path='/GestionBib' Component={GestionBib}></Route>
-              <Route path='/gestionCalendar' Component={GestionCalendar}></Route>
-              <Route path='/gestionClub' Component={GestionClub}></Route>
-              <Route path='/gestionEmplois' Component={GestionEmploi}></Route>
-              <Route path='/gestionNote' Component={GestionNote}></Route>
-              <Route path='/gestionPresence' Component={GestionPresence}></Route>
-              <Route path='/gestionProfil' Component={GestionProfil}></Route>
->>>>>>> 1426ec7e16bdb441b3101bab392881f38458d100
 
             {/*Enseignant*/}
             <Route
--- src/components/Enseignant/Dashboard/Cours/AddCours.js
+++ src/components/Enseignant/Dashboard/Cours/AddCours.js
@@ -1,34 +1,49 @@
 import React, { useState } from "react";
 import {
   Box,
-  Button,
-  IconButton,
-  MenuItem,
   FormControl,
   InputLabel,
+  MenuItem,
+  Select,
+  Button,
 } from "@mui/material";
 import Sidebar from "../Sidebar";
 import Topbar from "../../../Global/Topbar";
 import { CssBaseline } from "@mui/material";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-function Cours() {
-  const [matiere, setMatiere] = React.useState("");
+import { IconButton } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { styled } from "@mui/material/styles";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { Block } from "@mui/icons-material";
 
-  const handleChangeMatiere = (event: SelectChangeEvent) => {
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
+function Cours() {
+  const [matiere, setMatiere] = useState("");
+  const [filiere, setFilière] = useState("");
+  const [type, setType] = useState("");
+
+  const handleChangeMatiere = (event) => {
     setMatiere(event.target.value);
   };
 
-  const [filiere, setFilière] = React.useState("");
-
-  const handleChangeFiliere = (event: SelectChangeEvent) => {
+  const handleChangeFiliere = (event) => {
     setFilière(event.target.value);
   };
 
-  const [type, setType] = React.useState("");
-
-  const handleChangeType = (event: SelectChangeEvent) => {
+  const handleChangeType = (event) => {
     setType(event.target.value);
   };
+
   return (
     <div
       style={{ margin: 0, padding: 0, height: "100vh", overflowX: "hidden" }}
@@ -37,81 +52,121 @@ function Cours() {
       <Box
         sx={{
           background: "#F2F2F7",
-          minHeight: "100vh",
+          minHeight: "200vh",
           display: "flex",
           flexDirection: "column",
         }}
       >
         <Topbar />
-      </Box>
-      <Box
-        display="flex"
-        flexGrow={1}
-        overflow="auto"
-        style={{ overflowX: "hidden" }}
-      >
-        <Sidebar flex="2" />
-        <Box marginLeft="25%" />
-        <FormControl sx={{ m: 1, minWidth: 100 }}>
-          <InputLabel id="demo-simple-select-autowidth-label">
-            Matière
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-autowidth-label"
-            id="demo-simple-select-autowidth"
-            value={matiere}
-            onChange={handleChangeMatiere}
-            autoWidth
-            label="Matière"
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Twenty</MenuItem>
-            <MenuItem value={21}>Twenty one</MenuItem>
-            <MenuItem value={22}>Twenty one and a half</MenuItem>
-          </Select>
-        </FormControl>
+        <Box
+          display="flex"
+          flexGrow={1}
+          overflow="auto"
+          style={{ overflowX: "hidden" }}
+        >
+          <Sidebar flex="2" />
 
-        <FormControl sx={{ m: 1, minWidth: 100 }}>
-          <InputLabel id="demo-simple-select-autowidth-label">
-            Filière
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-autowidth-label"
-            id="demo-simple-select-autowidth"
-            value={filiere}
-            onChange={handleChangeFiliere}
-            autoWidth
-            label="Filière"
+          <Box marginLeft="25%" />
+          <Box
+            display={"block"}
+            width="calc(75% - 25%)"
+            height="520px"
+            marginRight="10%"
+            marginLeft={"140px"}
+            marginTop="4%"
+            justifyItems={"center"}
+            sx={{
+              border: "none",
+              backgroundColor: "white", // Changez la couleur du fond du tableau en blanc
+              borderRadius: 5,
+              boxShadow: "0px 20px 30px rgba(0, 0, 0, 0.1)",
+            }}
           >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Twenty</MenuItem>
-            <MenuItem value={21}>Twenty one</MenuItem>
-            <MenuItem value={22}>Twenty one and a half</MenuItem>
-          </Select>
-        </FormControl>
+            <Box paddingTop={"30px"} display={"flex"} justifyContent={"center"}>
+              <FormControl sx={{ m: 1, minWidth: 100, marginRight: "70px" }}>
+                <InputLabel id="matiere-label">Matière</InputLabel>
+                <Select
+                  labelId="matiere-label"
+                  id="matiere-select"
+                  value={matiere}
+                  onChange={handleChangeMatiere}
+                  autoWidth
+                  label="Matière"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Twenty</MenuItem>
+                  <MenuItem value={21}>Twenty one</MenuItem>
+                  <MenuItem value={22}>Twenty one and a half</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl sx={{ m: 1, minWidth: 100, marginRight: "70px" }}>
+                <InputLabel id="filiere-label">Filière</InputLabel>
+                <Select
+                  labelId="filiere-label"
+                  id="filiere-select"
+                  value={filiere}
+                  onChange={handleChangeFiliere}
+                  autoWidth
+                  label="Filière"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Twenty</MenuItem>
+                  <MenuItem value={21}>Twenty one</MenuItem>
+                  <MenuItem value={22}>Twenty one and a half</MenuItem>
+                </Select>
+              </FormControl>
 
-        <FormControl sx={{ m: 1, minWidth: 100 }}>
-          <InputLabel id="demo-simple-select-autowidth-label">Type</InputLabel>
-          <Select
-            labelId="demo-simple-select-autowidth-label"
-            id="demo-simple-select-autowidth"
-            value={type}
-            onChange={handleChangeType}
-            autoWidth
-            label="Filière"
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Twenty</MenuItem>
-            <MenuItem value={21}>Twenty one</MenuItem>
-            <MenuItem value={22}>Twenty one and a half</MenuItem>
-          </Select>
-        </FormControl>
+              <FormControl sx={{ m: 1, minWidth: 100 }}>
+                <InputLabel id="type-label">Type</InputLabel>
+                <Select
+                  labelId="type-label"
+                  id="type-select"
+                  value={type}
+                  onChange={handleChangeType}
+                  autoWidth
+                  label="Type"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Twenty</MenuItem>
+                  <MenuItem value={21}>Twenty one</MenuItem>
+                  <MenuItem value={22}>Twenty one and a half</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <Box
+              height={"250px"}
+              width={"350px"}
+              marginTop={5}
+              marginLeft={"200px"}
+              display={"flex"}
+              justifyContent={"center"}
+              sx={{ border: "2px solid grey" }}
+            >
+              <IconButton color="primary">
+                <AddCircleIcon sx={{ width: "50px", height: "50px" }} />
+              </IconButton>
+            </Box>
+            <Box display={"flex"} justifyContent={"center"} marginTop={5}>
+              <Button
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                sx={{ width: "350px" }}
+                startIcon={<CloudUploadIcon />}
+              >
+                Ajouter
+                <VisuallyHiddenInput type="file" />
+              </Button>
+            </Box>
+          </Box>
+        </Box>
       </Box>
     </div>
   );
